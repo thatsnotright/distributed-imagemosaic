6fef33269425981d376493ea81b9f3f44e645665
chris_grosshans@comcast.net <chris_grosshans@comcast.net@dc0180d3-155e-3b92-1ca6-06e9625264dc>
2011-03-15 23:28:16 +0000

    Accounting for Date/Time line when selecting images.

--- trunk/MosaicGrailsApp/grails-app/services/edu/cu/csci5673/caete/ImageHeaderService.groovy
+++ trunk/MosaicGrailsApp/grails-app/services/edu/cu/csci5673/caete/ImageHeaderService.groovy
@@ -5,17 +5,10 @@ class ImageHeaderService {
      * They should be the same length.
      * The sum of the elements should not exceed Coordinate.DEGREES_LONGITUDE
      */
-    protected int[] LONG_EXPANSION = [1000,10000,100000] 
-    protected int[] LAT_EXPANSION  = [500,5000,50000]
+    public static List<Integer> LONG_EXPANSION = [1000,10000,100000]
+    public static List<Integer> LAT_EXPANSION  = [500,5000,50000]
 
     static expose=['cxf']
-
-//    boolean saveImage(ImageHeader saveMe){
-//        // TODO save me is null in this signature.
-//        Id and version are also added to the wsdl
-//        saveMe.save()
-//        return false
-//    }
 
     /* Save a new image */
     void saveImage(String url, Integer longitude, Integer latitude,
@@ -30,31 +23,87 @@ class ImageHeaderService {
      */
     Set<ImageHeader> selectImagesNearLocation(Integer longitude, Integer latitude, Integer numImagesToSelect){
         
-        return ImageHeader.list(max:numImagesToSelect)
+        //return ImageHeader.list(max:numImagesToSelect)
 
+        /* progressivly select images in larger search areas until
+         * numImagesToSelect images are found.
+         */
+        Set<ImageHeader> results = new HashSet<ImageHeader>()
+        Coordinate lowerBounds = new Coordinate(longitude,latitude)
+        Coordinate upperBounds = new Coordinate(longitude,latitude)
+        for (int pos = 0; pos < LONG_EXPANSION.size-1; pos++) {
+            if (results.size()>=numImagesToSelect){
+                break; // found enough images
+            }
+            
+            /* Widen search area then query DB
+             * Note we never search for a single coordinate.
+             * http://grails.org/doc/latest/guide/single.html#5.4%20Querying%20with%20GORM section 5.4.2
+             */
+            lowerBounds = lowerBounds.add(LONG_EXPANSION[pos]*-1,LAT_EXPANSION[pos]*-1)
+            upperBounds = upperBounds.add(LONG_EXPANSION[pos],LAT_EXPANSION[pos])
+            if (isCrossingDateBoundry(lowerBounds.getLongitude(), upperBounds.getLongitude())){
+                // need two selects as the area crosses the international date line
+                selectImages(results, numImagesToSelect,
+                    lowerBounds.getLongitude(), Coordinate.MAX_LONGITUDE,
+                    lowerBounds.getLatitude(), upperBounds.getLatitude())
+                selectImages(results, numImagesToSelect,
+                    Coordinate.MIN_LONGITUDE, upperBounds.getLongitude(),
+                    lowerBounds.getLatitude(), upperBounds.getLatitude())
+            } else {
+                selectImages(results, numImagesToSelect,
+                    lowerBounds.getLongitude(), upperBounds.getLongitude(),
+                    lowerBounds.getLatitude(), upperBounds.getLatitude())
+            }
+            
+        }
+        if (results.size()<numImagesToSelect){
+            // If we have not found enough images select from all images in DB
+            Set<ImageHeader> searchResults = ImageHeader.list(max:numImagesToSelect)
+            mergeImages(results, searchResults, numImagesToSelect)
+        }
+        return results;
+    }
 
-//        Set<ImageHeader> results = new HashSet<ImageHeader>()
-//        /* progressivly select images in larger search areas until
-//         * numImagesToSelect images are found.
-//         */
-//        Coordinate oldUpperBounds = new Coordinate(longitude,latitude)
-//        Coordinate oldLowerBounds = new Coordinate(longitude,latitude)
-//        for (int pos = 0; pos < LONG_EXPANSION.length()-1; pos++) {
-//            if (numImagesToSelect<=0){
-//                break; // found enough images
-//            }
-//
-//            // update maximum to widen search
-//            Coordinate newUpperBounds = oldUpperBounds.add(LONG_EXPANSION[pos],LAT_EXPANSION[pos])
-//            Coordinate newLowerBounds = oldUpperBounds.add(LONG_EXPANSION[pos]*-1,LAT_EXPANSION[pos]*-1)
-//
-//            // TODO IMPLEMENT SEARCH
-//            // to query http://grails.org/doc/latest/guide/single.html#5.4%20Querying%20with%20GORM section 5.4.2
-//
-//            // update minimums so we do not reselect old images.
-//            oldUpperBounds = newUpperBounds
-//            oldUpperBounds = newLowerBounds
-//        }
-//        return results;
+    /* execute select */
+    private void selectImages(Set<ImageHeader> results, Integer numImagesToSelect,
+        Integer westLongitude, Integer eastLongitude,
+        Integer southLatitude, Integer northLatitude){
+        def critera = ImageHeader.createCriteria()
+        Set<ImageHeader> searchResults = critera {
+                between("longitude", westLongitude, eastLongitude)
+                between("latitude", southLatitude, northLatitude)
+                maxResults(numImagesToSelect)
+         }
+         mergeImages(results, searchResults, numImagesToSelect)
+    }
+
+    /* return true if you cross the International date/time boundry when
+     * traveling from west to east given the two longitudes.
+     */
+    private boolean isCrossingDateBoundry(Integer westLongitude, Integer eastLongitude){
+        // if west is positive and east is negative
+        if (westLongitude>0 && eastLongitude<0){
+            return true
+        }
+        return false
+    }
+    /*
+     * merge addMe with addToMe.  Do not allow the addToMe's size to exceed
+     * numImagesToSelect
+     */
+    private void mergeImages(Set<ImageHeader> addToMe, Set<ImageHeader> addMe, Integer numImagesToSelect){
+        if (addToMe==null){
+            addToMe = new HashSet<ImageHeader>()
+        }
+
+        if (addMe!=null){
+            for (ImageHeader header : addMe){
+                if (addToMe.size()>=numImagesToSelect){
+                    break; // Enough images found stop adding images
+                }
+                addToMe.add(header)
+            }
+        }
     }
 }
--- trunk/MosaicGrailsApp/test/integration/edu/cu/csci5673/caete/ImageHeaderServiceIntegrationTests.groovy
+++ trunk/MosaicGrailsApp/test/integration/edu/cu/csci5673/caete/ImageHeaderServiceIntegrationTests.groovy
@@ -12,19 +12,144 @@ class ImageHeaderServiceIntegrationTests extends GroovyTestCase {
     }
 
     void testSimpleSelectImagesNearLocation() {
-        removeAllImageHeaders();
+        // Add some test data
+        removeAllImageHeaders();        // cleanup previous data
         ImageHeaderService service = new ImageHeaderService()
-
-
         service.saveImage("URL",2,2,2,2,2)
         service.saveImage("URL2",2,2,2,2,2)
+
+        // make sure we respect numImagesToSelect param
         Set<ImageHeader> results = service.selectImagesNearLocation(2,2,100)
-        assertEquals 2,results.size()
+        assertEquals 2,results.size()                       // select all images
         results = service.selectImagesNearLocation(2,2,1)
-        assertEquals 1,results.size()
-        removeAllImageHeaders()
-        results = service.selectImagesNearLocation(2,2,100)
-        assertEquals 0,results.size()
+        assertEquals 1,results.size()                       // select 1 image
+        results = service.selectImagesNearLocation(2,2,0)
+        assertEquals 0,results.size()                       // select no images
+        results = service.selectImagesNearLocation(2,2,-1)
+        assertEquals 0,results.size()                       // select no images
+    }
+
+    /* test searching for images using longitude */
+    void testSelectImagesNearLocationLongitudeRangeExpansion() {
+        // Add some test data
+        Integer base = 0;
+        removeAllImageHeaders();        // cleanup previous data
+        ImageHeaderService service = new ImageHeaderService()
+        service.saveImage("firstSelect",base,0,2,2,2)
+        service.saveImage("secondSelect",getCoordInSelect(base,true,2,true),0,2,2,2)
+        service.saveImage("finalSelect",getCoordInSelect(base,true,99,false),0,2,2,2)
+
+        Set<ImageHeader> results = service.selectImagesNearLocation(base,0,1)
+        assertContainsAllUrls(results, "firstSelect")
+        results = service.selectImagesNearLocation(base,0,2)
+        assertContainsAllUrls(results, "firstSelect","secondSelect")
+        results = service.selectImagesNearLocation(base,0,3)
+        assertContainsAllUrls(results, "firstSelect","secondSelect","finalSelect")
+    }
+
+    /* test searching for images using latitude */
+    void testSelectImagesNearLocationLatitudeRangeExpansion() {
+        // Add some test data
+        Integer base = 0;
+        removeAllImageHeaders();        // cleanup previous data
+        ImageHeaderService service = new ImageHeaderService()
+        service.saveImage("firstSelect",0,base,2,2,2)
+        service.saveImage("secondSelect",0,getCoordInSelect(base,false,2,false),2,2,2)
+        service.saveImage("finalSelect",0,getCoordInSelect(base,true,99,true),2,2,2)
+
+        Set<ImageHeader> results = service.selectImagesNearLocation(0,base,1)
+        assertContainsAllUrls(results, "firstSelect")
+        results = service.selectImagesNearLocation(0,base,2)
+        assertContainsAllUrls(results, "firstSelect","secondSelect")
+        results = service.selectImagesNearLocation(0,base,3)
+        assertContainsAllUrls(results, "firstSelect","secondSelect","finalSelect")
+    }
+
+    /* test searching for images using longitude AND wrapping around the date/time line*/
+    void testSelectImagesNearLocationInternationalDateTimeBoundryAsian() {
+        // Add some test data
+        Integer base = Coordinate.MAX_LONGITUDE-2; // just on asian side of boundry
+        removeAllImageHeaders();        // cleanup previous data
+        ImageHeaderService service = new ImageHeaderService()
+        service.saveImage("finalSelectPositive",getCoordInSelect(base,true,99,true),0,2,2,2)
+        service.saveImage("finalSelectNegative",getCoordInSelect(base,true,99,false),0,2,2,2)
+        service.saveImage("firstSelect",base,0,2,2,2)
+        service.saveImage("secondSelectPositive",getCoordInSelect(base,true,2,true),0,2,2,2)
+        service.saveImage("secondSelectNegative",getCoordInSelect(base,true,2,false),0,2,2,2)
+        
+        // should select the three items closest to the boundry.  
+        Set<ImageHeader> results = service.selectImagesNearLocation(base,0,3)
+        assertContainsAllUrls(results, "firstSelect","secondSelectPositive","secondSelectNegative")
+    }
+
+    /* test searching for images using longitude AND wrapping around the date/time line*/
+    void testSelectImagesNearLocationInternationalDateTimeBoundryAmerican() {
+        // Add some test data
+        Integer base = Coordinate.MIN_LONGITUDE+2; // just on american side of boundry
+        removeAllImageHeaders();        // cleanup previous data
+        ImageHeaderService service = new ImageHeaderService()
+        service.saveImage("finalSelectPositive",getCoordInSelect(base,true,99,true),0,2,2,2)
+        service.saveImage("finalSelectNegative",getCoordInSelect(base,true,99,false),0,2,2,2)
+        service.saveImage("firstSelect",base,0,2,2,2)
+        service.saveImage("secondSelectPositive",getCoordInSelect(base,true,2,true),0,2,2,2)
+        service.saveImage("secondSelectNegative",getCoordInSelect(base,true,2,false),0,2,2,2)
+
+        // should select the three items closest to the boundry.
+        Set<ImageHeader> results = service.selectImagesNearLocation(base,0,3)
+        assertContainsAllUrls(results, "firstSelect","secondSelectPositive","secondSelectNegative")
+    }
+
+    /*
+     *  get a coordinate that falls in given search expansion range 
+     *  baseCoord = center of search
+     *  longitudeExpansion = if true then use ImageHeaderService.LONG_EXPANSION else ImageHeaderService.LAT_EXPANSION
+     *  selectRound = Round you want to select in (1 based)
+     *  positive = true if you want to move coordinate "after" baseCoord 
+     */
+    private Integer getCoordInSelect(int baseCoord, boolean longitudeExpansion, int selectRound, boolean positive){
+        // determine if we want to move coordinate forward or backwards
+        int sign = 1
+        if (!positive){sign = -1}
+
+        // determine which expansion to use
+        List<Integer> expansion = ImageHeaderService.LONG_EXPANSION
+        if (!longitudeExpansion){
+            expansion = ImageHeaderService.LAT_EXPANSION
+        }
+
+        // determine coordinate adjustment.
+        int index = selectRound-2
+        int adjustment = 1*sign
+        if (expansion.size()>index){
+            adjustment += (expansion.getAt(index)*sign) // if selectRound in bounds
+        } else {// if selectRound index out of bounds put last
+            adjustment += (expansion.getAt(expansion.size()-1)*sign)
+        }
+
+        Coordinate coord = null
+        Integer retVal = 0
+        if (longitudeExpansion){
+            coord = new Coordinate(baseCoord,0)
+            coord = coord.add(adjustment, 0)
+            retVal = coord.getLongitude()
+        } else {
+            coord = new Coordinate(0,baseCoord)
+            coord = coord.add(0, adjustment)
+            retVal = coord.getLatitude()
+        }
+        return retVal
+    }
+
+    /* make sure results contain all urls in list */
+    private void assertContainsAllUrls(Set<ImageHeader> results, String... expectedUrls){
+        assertNotNull results
+        Set<String> actualUrls = new ArrayList<String>()
+        results.each {
+            actualUrls.add(it.getUrl())
+        }
+        String msg = "Actual   : "+actualUrls+" Expected :"+expectedUrls
+        assertEquals msg,results.size(),expectedUrls.length // lists must be same length
+        assertTrue msg,actualUrls.containsAll(expectedUrls)
     }
 
     /* clear all items from ImageHeader table */
